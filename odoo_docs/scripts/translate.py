8a967658eba2e59c8b2a95757a9c3a4d70a70efa
moustafa shahin <moustafashahin122@gmail.com>
2024-10-08 18:00:37 +0300

    up

--- odoo_docs/scripts/translate.py
+++ odoo_docs/scripts/translate.py
@@ -1,4 +1,86 @@
 # ask for the source and destination paths from user input
+
+
+# import json
+# import re
+
+# def remove_comments(json_like_str):
+#     """Remove comments from VS Code config (// style)."""
+#     # Remove single-line comments
+#     json_like_str = re.sub(r'//.*?\n', '\n', json_like_str)
+#     # Remove multi-line comments
+#     json_like_str = re.sub(r'/\*.*?\*/', '', json_like_str, flags=re.S)
+    
+#     # Remove trailing commas before closing braces or brackets
+#     json_like_str = re.sub(r',\s*([\]}])', r'\1', json_like_str)
+    
+#     # Remove trailing commas in lists (new)
+#     json_like_str = re.sub(r',\s*([}\]])', r'\1', json_like_str)
+
+#     return json_like_str
+
+# def load_vscode_config(file_path):
+#     """Load a VS Code configuration file by removing comments."""
+#     with open(file_path, 'r') as file:
+#         content = file.read()
+    
+#     # Remove the comments before parsing the content
+#     cleaned_content = remove_comments(content)
+    
+#     # Now parse the cleaned JSON-like string
+#     print(cleaned_content)
+#     config = json.loads(cleaned_content)
+#     return config
+
+
+
+
+
+# import subprocess
+
+# # Define the path to your odoo-bin file
+# odoo_bin_path = '/path/to/odoo/odoo-bin'
+
+# # Define the parameters and options for running Odoo
+# args = [
+#     odoo_bin_path,             # Path to odoo-bin
+#     '-c', '/path/to/odoo.conf'  # Path to your Odoo configuration file
+# ]
+
+# # Start Odoo using subprocess
+# process = subprocess.Popen(args)
+
+# # Optional: Wait for the process to complete
+# process.wait()
+
+
+
+
+
+
+
+
+
+# # Example usage
+# config_path = '/opt/work_code/odoo_15/.vscode/launch.json'  # Replace with the actual path
+# config = load_vscode_config(config_path)
+
+# # Access parts of the configuration
+# print(config["version"])  # Example to print the version
+# print(config["configurations"][0]["name"])  # Example to print the configuration name
+
+
+
+
+
+
+
+
+
+
+
+
+
 import os
 import shutil
 from translate_po.main import run
