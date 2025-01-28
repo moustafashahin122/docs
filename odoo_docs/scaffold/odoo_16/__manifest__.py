f5bbfbdcf54057854492e44661f71e6c992bc8d2
mostafa.shahin@intalio.com <mostafa.shahin@intalio.com>
2025-01-28 15:48:25 +0200

    up

---
+++ odoo_docs/scaffold/odoo_16/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Contract Change Request",
+
+    'summary': "This module adds the functionality of requesting a contract change",
+
+    'description': """
+This module adds the functionality of requesting a contract change
+    """,
+
+      'author': "Shahin",
+    'website': "Shahin",
+    'category': 'Contract Management',
+    'version': '16.0.1.0.0',
+    'installable': True,
+    'application': False,
+    'license': 'Other proprietary',
+
+    
+    
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    'data': [
+        # 'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+
+}
+
